dc076da3ed28a6a9b2990577819b61a09faaab7b
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:37:53 +0000

    Bucle

---
+++ bucle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Cantidad de niños: ");
+    for (int i = 0; i < 5; i++) {
+        System.out.println(i + "niño");
+    }
+
+  }
+}
+
