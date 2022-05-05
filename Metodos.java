56f408a3e965ddc7a672253743ab52f701fabbe1
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:54:26 +0000

    Metodos

---
+++ Metodos.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int x=5,y=2;
+    String Javier;
+    public static int suma(int x, int y) {
+        return x + y;
+    }
+
+    public static double division(int x,int y) {
+        return x/y;
+    }
+    public static void NombreEdad(String x, int y){
+        System.out.println("El alumno " + x + " tiene " + y + " años.")
+    }
+
+    
+  }
+}
+
