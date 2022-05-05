4feaa812f34a32090d41694d0229a252b2e62ba3
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:05:55 +0000

    Operadores

---
+++ Operadores.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = 50;
+    int b = 30;
+    int suma = a + b;
+    int multiplicacion = a * b;
+    int resta = a - b;
+    double division = a / b;
+    int modulo = a % b;
+    System.out.println("Primer valor = " + a);
+    System.out.println("Segundo valor = " + b);
+    System.out.println("Suma = " + suma);
+    System.out.println("Resta = " + resta);
+    System.out.println("Multiplicacion = " + multiplicacion);
+    System.out.println("Division = " + division);
+    System.out.println("Modulo = " + modulo)
+    
+  }
+}
+
