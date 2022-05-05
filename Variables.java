f0327f3ee8b4a1acc7492f979cbad3054d213674
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 21:52:31 +0000

    Variables

---
+++ Variables.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+     String texto='Jhon';
+     int numero=12; 
+     float numeroDecimal=12.3;
+     char soloLetra='a';
+     boolean vOf=true;
+     String sum=texto+soloLetra;
+     float suma = numero + numeroDecimal;
+     System.out.println(texto+soloLetra=sum);
+     System.out.println(numero+numeroDecimal=suma);
+  }
+}
+
