47ce7122c3d173cb367ea8f9416b981093542c5e
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:32:55 +0000

    Conmutador

---
+++ conmutador.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+     int puesto = 2;
+     switch (day) {
+       case 1:
+          System.out.println("Primer Puesto");
+          break;
+       case 2:
+          System.out.println("Segundo Puesto");
+          break;
+       case 3:           
+          System.out.println("Tercer Puesto");
+          break;
+       case 4:
+          System.out.println("Cuarto Puesto");
+          break;
+     }    
+  }
+
+}
+
