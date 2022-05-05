3be8fc5813f3c3f7d38474312732f3c5ec1fc014
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 21:41:20 +0000

    Oraciones

---
+++ Oraciones.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+      System.out.println("Mi nombre es Frank's Vilca Quispe"); 
+      System.out.println("Estoy estudiando Ingenieria de Sistemas"); 
+  }
+}
+
