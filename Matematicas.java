1c29b57bb2fd5f7c737b3dae7142a6ece93ba37a
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:20:38 +0000

    Matematicas

---
+++ Matematicas.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+   
+    System.out.println("El maximo valor: " + Math.max(5,10));
+    System.out.println("El minimo valor: " + Math.min(5,10));
+    System.out.println("La raiz Cuadrada: " + Math.sqrt(64));
+    System.out.println("El valor absoluto: " + Math.abs(-4.8));
+    System.out.println("Un valor random entre 1 y 100: " + Math.random()*100+1);
+    
+  }
+}
+
