0093371308ca0123a0881974ffd38aa95223da3a
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 21:45:28 +0000

    Comentarios

---
+++ Comentarios.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    //Este es un comentario en una sola linea
+    System.out.println("Esto no es un comentario");
+    
+    /*Esto se utiliza para comentario en multilineas
+    System.out.println("Esto sigue siendo un comentario");
+    Aqui termina el comentario */ 
+  }
+}
+
