1051a9591bd62c2d7d05cfb91efa17b5d1966d4f
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:00:23 +0000

    Conversion

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double valor = 9.56; 
+    int newValor = valor; //El valor se convertira en 9
+    
+    System.out.println(valor);
+    System.out.println(newValor);
+  }
+}
+
