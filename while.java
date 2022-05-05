37651294d1d04ec4b0e317d6e3caf6da1702df42
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:35:24 +0000

    Ciclo While

---
+++ while.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int i = 0;
+    while (i < 10) {
+        System.out.println("puesto: " + i);
+          i++;
+    }
+    
+  }
+}
+
