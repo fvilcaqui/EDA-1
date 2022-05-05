779e56e3cefccc1eaf74e4695ee14d23d2eceeff
Frank's Javier Vilca Quispe <fvilcaqui@unsa.edu.pe>
2022-05-05 22:26:19 +0000

    ifElse

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int edad = 20;
+
+    if (edad < 18) {
+        System.out.println("Aun eres menor de edad");
+    } else {
+        System.out.println("Ya eres mayor de edad");
+    }
+  }
+}
+
